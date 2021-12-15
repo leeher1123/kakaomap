eb2df01d657c400fbd673c58f6fc7ced828cc9ba
leeher1123 <leeher1123@naver.com>
2021-12-15 11:06:27 +0900

    Revert "기본 환경 세팅"
    This reverts commit 47fccfbe944861266aafbb87efbbf7e148e1e4fb.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Home from './page/Home';
-import { GlobalStyle } from './style/GlobalStyle';
-
-const App = () => {
-  const a = 1;
+function App() {
   return (
-    <Container>
-      <GlobalStyle />
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
-
-const Container = styled.div`
-
-`;
+}
 
 export default App;
